acd7588d2f1ad49446cd405a22f05bc247153d49
italo yeltsin <br.yeltsin@gmail.com>
2020-07-14 15:23:43 -0300

    deprecated

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,23 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-var Packager = require('./packager');
-var Deployer = require('./deployer');
-var Creator = require('./creator');
-import {login} from './login'; 
-import { register } from './register';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	/**
-	 * Create new Project
-	 */
-	let disposable = vscode.commands.registerCommand('extension.createNewProject', function() {
-		Creator.createNewProject(context);
-	});
-	context.subscriptions.push(disposable);
-	
-	/**
-	 * Package
-	 */
-	disposable = vscode.commands.registerCommand('extension.package', Packager.package);
-	context.subscriptions.push(disposable);
-
-	/**
-	 * Deploy 
-	 */
-	disposable = vscode.commands.registerCommand('extension.deploy', Deployer.deploy);
-	context.subscriptions.push(disposable);
-
-	/**
-	 * Deploy and Run
-	 */
-	disposable = vscode.commands.registerCommand('extension.deployAndRun', Deployer.deployAndRun);
-	context.subscriptions.push(disposable);
-	
-	
-	/**
-	 * Login
-	 */
-	disposable = vscode.commands.registerCommand('extension.login', login);
-	context.subscriptions.push(disposable);
-
-	/**
-	 * Register
-	 */
-	disposable = vscode.commands.registerCommand('extension.register', register);
-	context.subscriptions.push(disposable);
+export async function activate(context: vscode.ExtensionContext) {
+	let message = 
+	"TotalCross Extension: the current had its ownership move to the official TotalCross publisher." +
+	" Please unninstall this extension and install the new extension from the official publisher.";
+	let installExt = "Install new extension";
+	let removeCurr = "Remove current extension";
+	let result = await vscode.window.showWarningMessage(message, installExt, removeCurr);
+	if(result === installExt) {
+		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('vscode:extension/totalcross.totalcross'));
+		result = await vscode.window.showWarningMessage(message, removeCurr);
+	}
+	if(result === removeCurr) {
+		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('vscode:extension/italo.totalcross'));
+	}	
 }
 
 // this method is called when your extension is deactivated
--- src/validators/deployer.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-
-exports.user = function(value: string) {
-        if(!value) {
-            return 'user is required!';
-        }
-        return null;
-};
